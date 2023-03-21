65277d9ebfa6222bb3fa6fb7f7b13c65260955c4
iiNani <akmeduri101@gmail.com>
2023-03-21 03:18:36 -0700

    Initial commit

---
+++ src/main/java/net/iinani/healththeft/HealthTheftClient.java
@@ -0,0 +1,10 @@
+package net.iinani.healththeft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HealthTheftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
